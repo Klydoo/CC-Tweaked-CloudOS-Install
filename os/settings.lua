cb5d83d9a35104fbcae88b74d91e478b17e436de
Klydo <matvei7142@gmail.com>
2026-07-14 16:33:55 +0200

    Update settings.lua

--- os/settings.lua
+++ os/settings.lua
@@ -6,24 +6,24 @@ ui.HeaderInfo = {
 }
 ui.Title = "Settings Menu"
 ui.StartOption = 1
-ui.BackOption = 3
+ui.BackOption = 1
 ui.Options = {
-    [1] = "Uninstall OS",
+    [1] = "Back",
     [2] = "Reset Save Config",
-    [3] = "Back",
+    [3] = "Uninstall OS",
 }
 
 -- Display
 ui:Draw()
 ui:OnSelect(function(option)
     if option == 1 then
-        shell.run("/os/Settings/uninstallOS.lua")
+        shell.run("/os/menu.lua")
         return 1
     elseif option == 2 then
         shell.run("/os/Settings/resetSaveConfig.lua")
         return 1
     elseif option == 3 then
-        shell.run("/os/menu.lua")
+        shell.run("/os/Settings/uninstallOS.lua")
         return 1
     end
 end)
